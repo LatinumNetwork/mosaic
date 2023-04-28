518e84e2617e58c2013d2aaad2cac1db96e48222
alderr <vernonmensah@gmail.com>
2023-04-28 01:46:26 -0400

    more setup and template

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -6,8 +6,7 @@ export type ButtonProps = {
   size: string;
   backgroundColor?: string;
   label: string;
-  [x: string]: any;
-}
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 /**
  * Primary UI component for user interaction
@@ -16,7 +15,7 @@ export const Button = ({ primary, size, label, ...props }: ButtonProps ) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
-      type="button"
+      type={"button"}
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       {...props}
     >
