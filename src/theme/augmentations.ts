be18ee7b7d3e605940f2c4531db44ce9311dac72
Juan <desarrollojuseca@gmail.com>
2023-09-13 16:57:21 -0600

    removed unused change

--- src/theme/augmentations.ts
+++ src/theme/augmentations.ts
@@ -8,7 +8,7 @@ declare module '@mui/material/styles' {
     '200'?: string;
     '300'?: string;
     '400'?: string;
-    '500': string;
+    '500'?: string;
     '600'?: string;
     '700'?: string;
     '800'?: string;
