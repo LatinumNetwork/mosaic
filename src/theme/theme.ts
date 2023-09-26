758b418493c1288bb63ee878b4d8d587e94a8492
Jefferson Arrubla <jarrubla@collagegroup.com>
2023-09-26 10:24:43 -0500

    Added Nunito as font

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,4 @@
+import '@fontsource/nunito-sans';
 import {
   CustomColorShades,
   CustomPaletteOptions,
@@ -42,7 +43,10 @@ export const palette: PaletteOptions = {
 const themeOptions: ThemeOptions = {
   palette,
   typography: {
-    fontFamily: "'Roboto', sans-serif",
+    allVariants: {
+      fontFamily: "'Nunito', sans-serif",
+    },
+    fontFamily: "'Nunito', sans-serif",
     fontSize: 16,
     h4: undefined,
     h5: undefined,
@@ -90,7 +94,7 @@ const themeOptions: ThemeOptions = {
         },
       },
       defaultProps: {
-        fontFamily: "'Roboto', sans-serif",
+        fontFamily: "'Nunito', sans-serif",
         // by default, new variants will render as span
         variantMapping: {
           b1: 'p',
