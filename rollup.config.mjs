53a3eec56702992cba6ac9ff21ec67a4ab6dd792
Jefferson Arrubla <jarrubla@collagegroup.com>
2023-09-27 11:54:04 -0500

    Added MuiCssBaseline for fonts and added rollup-plugin-import-css package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import css from "rollup-plugin-import-css";
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -25,6 +26,7 @@ export default [
         requireReturnsDefault: 'namespace',
       }),
       typescript({ tsconfig: './tsconfig.json' }),
+      css() 
     ],
   },
   {
