1d4039580c8f0bebf2701a43186e5a567692f95e
Juan <desarrollojuseca@gmail.com>
2023-09-27 18:19:08 -0600

    added palette

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -37,6 +37,17 @@ const generatePalette = (
 export const palette: PaletteOptions = {
   ...generatePalette(ui, 'ui'),
   ...generatePalette(collage, 'collage'),
+  mode: 'light',
+  primary: { main: '#954661', dark: '#7b1839', light: '#a7647a' },
+  secondary: { main: '#c16f8b', dark: '#b14b6e', light: '#cb869e' },
+  error: { main: '#CC3333', light: '#d65c5c', dark: '#ba2e2e' },
+  warning: { main: '#d5902f', light: '#ea9e34', dark: '#a67025' },
+  success: { main: '#18914c', light: '#46a770', dark: '#168445' },
+  text: {
+    primary: '#333333',
+    secondary: '#5c5c5c',
+    disabled: '#b4b4b4',
+  },
 };
 
 const themeOptions: ThemeOptions = {
@@ -109,15 +120,6 @@ const themeOptions: ThemeOptions = {
         root: {
           '&:hover': {
             boxShadow: 'none',
-          },
-        },
-      },
-    },
-    MuiMenuItem: {
-      styleOverrides: {
-        root: {
-          '&.Mui-selected': {
-            backgroundColor: '#954661', //CollageRed[500]
           },
         },
       },
