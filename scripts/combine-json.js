37a30ad060d89c821798808b3a38465e7418d453
Juan <desarrollojuseca@gmail.com>
2024-03-26 09:34:23 -0600

    combined json script test with node

---
+++ scripts/combine-json.js
@@ -0,0 +1,9 @@
+import collage from '../src/colors/collage.ts';
+import ui from '../src/colors/ui.ts';
+
+const combined = {
+  collage: collage,
+  ui: ui,
+};
+
+console.log(JSON.stringify(combined));
