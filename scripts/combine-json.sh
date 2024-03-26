0827a4085d7b630c059d454c890563dafa5f7b59
Juan <desarrollojuseca@gmail.com>
2024-03-26 09:15:04 -0600

    combined json script test

--- scripts/combine-json.sh
+++ scripts/combine-json.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd src/colors
-
 # Delete default export from JSON
 json_collage=$(sed '/export default/d' ../src/colors/collage.ts | jq -c .)
 json_ui=$(sed '/export default/d' ../src/colors/ui.ts | jq -c .)
@@ -9,7 +7,7 @@ json_ui=$(sed '/export default/d' ../src/colors/ui.ts | jq -c .)
 # Combine JSON
 combined_json="{\"collage\": $json_collage, \"ui\": $json_ui}"
 
-cd ../..
+echo "{ \"collage\": $json_collage, \"ui\": $json_ui }"
 
 # Update AWS ssm parameter
 aws ssm put-parameter --name "/config/COLLAGE_UI_COLORS" --type "String" --value "$combined_json" --overwrite
