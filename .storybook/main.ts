990d3f41ce3a7b8159bd02d75a69c2d431d57a35
Vernon Mensah <vernonmensah@gmail.com>
2024-06-12 15:54:05 -0400

    new changes for dev workflow

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,31 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-    ],
-    typescript: {
-        check: false,
-        reactDocgen: 'react-docgen-typescript',
-        reactDocgenTypescriptOptions: {
-            shouldExtractLiteralValuesFromEnum: true,
-            propFilter: (prop) =>
-                prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-        },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook',
+  ],
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
-    framework: {
-        name: '@storybook/react-vite',
-        options: {},
-    },
-    async viteFinal(config) {
-        // Add your configuration here
-        return config;
-    },
-    docs: {
-        autodocs: 'tag',
-    },
+  },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config) {
+    // Add your configuration here
+    return config;
+  },
+  docs: {},
 };
 
 export default config;
