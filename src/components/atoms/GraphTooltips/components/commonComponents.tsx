2d16cceb5008065e4d7e83c7a36516ce981e6379
Vernon Mensah <vernonmensah@gmail.com>
2025-01-08 01:19:22 -0500

    use our typography

--- src/components/atoms/GraphTooltips/components/commonComponents.tsx
+++ src/components/atoms/GraphTooltips/components/commonComponents.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography, TypographyProps, styled } from '@mui/material';
+import { Box, styled } from '@mui/material';
+import { Typography, TypographyProps } from 'src/components';
 import { hexToRgba } from 'src/utils';
 
 export const Container = styled(Box)({
