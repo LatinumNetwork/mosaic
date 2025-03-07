f22b7732d796782386f6b808c4db0d904f6f7d76
Diego Farfan <diego_farfan20@hotmail.com>
2025-03-07 17:26:21 -0500

    Removed  custom colors from button component. Updated story format to the new version

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,9 +1,6 @@
-import { Box } from '@mui/material';
 import { Plus, TrashSimple } from '@phosphor-icons/react';
-import { Meta, StoryFn } from '@storybook/react';
-import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Button } from 'src/components';
-import { PaletteColors } from 'src/types';
 
 const meta: Meta<typeof Button> = {
   title: 'Atoms/Button',
@@ -12,139 +9,92 @@ const meta: Meta<typeof Button> = {
     variant: {
       control: {
         type: 'select',
-        options: [
-          'primary',
-          'secondary',
-          'tertiary',
-          'link',
-          'nav',
-          'outlined',
-          'text',
-          'contained',
-        ],
-      },
-      table: {
-        defaultValue: {
-          summary: 'primary',
-        },
+        options: ['primary', 'secondary', 'tertiary', 'link', 'nav', 'text'],
       },
     },
     size: {
-      control: { type: 'select', options: ['small', 'medium', 'large'] },
-      table: {
-        defaultValue: {
-          summary: 'small',
-        },
+      control: { type: 'radio' },
+      options: ['small', 'medium', 'large'],
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
       },
     },
   },
+  args: {
+    size: 'medium',
+    disabled: false,
+  },
   parameters: {
-    controls: { include: ['variant', 'size'] },
+    controls: { include: ['variant', 'size', 'disabled', 'children'] },
   },
 };
 
 export default meta;
 
-const Template: StoryFn<typeof Button> = ({
-  children,
-  ...args
-}): React.JSX.Element => <Button {...args} children={children} />;
+type Story = StoryObj<typeof Button>;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  variant: 'primary',
-  children: 'Primary Button',
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Primary Button',
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  variant: 'secondary',
-  children: 'Secondary Button',
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Button',
+  },
 };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-  variant: 'tertiary',
-  children: 'Tertiary Button',
+export const Tertiary: Story = {
+  args: {
+    variant: 'tertiary',
+    children: 'Tertiary Button',
+  },
 };
 
-export const Text = Template.bind({});
-Text.args = {
-  variant: 'text',
-  children: 'Text Button',
+export const Text: Story = {
+  args: {
+    variant: 'text',
+    children: 'Text Button',
+  },
 };
 
-export const Link = Template.bind({});
-Link.args = {
-  variant: 'link',
-  children: 'Link Button',
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Link Button',
+  },
 };
 
-export const Nav = Template.bind({});
-Nav.args = {
-  variant: 'nav',
-  children: 'Nav Button',
+export const Nav: Story = {
+  args: {
+    variant: 'nav',
+    children: 'Nav Button',
+  },
 };
 
-export const Disabled: StoryFn = () => {
-  return (
-    <Box display="flex" justifyContent="space-around">
-      <Button variant="primary" disabled>
-        Button
-      </Button>
-      <Button variant="secondary" disabled>
-        Button
-      </Button>
-      <Button variant="tertiary" disabled>
-        Button
-      </Button>
-    </Box>
-  );
+export const WithIconStart: Story = {
+  args: {
+    variant: 'primary',
+  },
+  render: (args) => (
+    <Button startIcon={<Plus size={18} />} {...args}>
+      Button
+    </Button>
+  ),
 };
 
-export const Colors: StoryFn = () => {
-  return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '8px' }}>
-      <Button color={PaletteColors.uiGray}>uiGray</Button>
-      <Button color={PaletteColors.collageRaspberry}>collageRaspberry</Button>
-      <Button color={PaletteColors.collageTeal}>collageTeal</Button>
-      <Button color={PaletteColors.collageCitron}>collageCitron</Button>
-      <Button color={PaletteColors.collageNavy}>collageNavy</Button>
-      <Button color={PaletteColors.audienceMulticulturalYellow}>
-        audienceMulticulturalYellow
-      </Button>
-      <Button color={PaletteColors.audienceGenerationsOrange}>
-        audienceGenerationsOrange
-      </Button>
-      <Button color={PaletteColors.audienceLgtbqPurple}>
-        audienceLgtbqPurple
-      </Button>
-      <Button color={PaletteColors.audienceParentsAndKidsGreen}>
-        audienceParentsAndKidsGreen
-      </Button>
-      <Button color={PaletteColors.audienceKidsGreen}>audiencekidsGreen</Button>
-    </Box>
-  );
-};
-
-export const WithIconStart: StoryFn<typeof Button> = (args) => (
-  <Button startIcon={<Plus size={18} />} {...args}>
-    Button
-  </Button>
-);
-
-WithIconStart.args = {
-  variant: 'primary',
-  color: PaletteColors.collageRaspberry,
-};
-
-export const WithIconEnd: StoryFn<typeof Button> = (args) => (
-  <Button endIcon={<TrashSimple size={20} />} {...args}>
-    Button
-  </Button>
-);
-
-WithIconEnd.args = {
-  variant: 'primary',
-  color: PaletteColors.collageRaspberry,
+export const WithIconEnd: Story = {
+  args: {
+    variant: 'primary',
+  },
+  render: (args) => (
+    <Button endIcon={<TrashSimple size={20} />} {...args}>
+      Button
+    </Button>
+  ),
 };
